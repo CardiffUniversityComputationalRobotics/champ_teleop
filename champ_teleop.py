e420836027ac7a97b024bd361d360655bb65a1e1
sasilva1998 <sasilva1998@gmail.com>
2022-10-22 13:01:31 +0100

    fix: reformat w/ zero vel pub

--- champ_teleop.py
+++ champ_teleop.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-#credits to: https://github.com/ros-teleop/teleop_twist_keyboard/blob/master/teleop_twist_keyboard.py
+# credits to: https://github.com/ros-teleop/teleop_twist_keyboard/blob/master/teleop_twist_keyboard.py
 
 from __future__ import print_function
 
-import roslib; roslib.load_manifest('champ_teleop')
+import roslib
+
+roslib.load_manifest("champ_teleop")
 import rospy
 
 from sensor_msgs.msg import Joy
@@ -15,12 +17,15 @@ import tf
 import sys, select, termios, tty
 import numpy as np
 
+
 class Teleop:
     def __init__(self):
-        self.velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
-        self.pose_lite_publisher = rospy.Publisher('body_pose/raw', PoseLite, queue_size = 1)
-        self.pose_publisher = rospy.Publisher('body_pose', Pose, queue_size = 1)
-        self.joy_subscriber = rospy.Subscriber('joy', Joy, self.joy_callback)
+        self.velocity_publisher = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+        self.pose_lite_publisher = rospy.Publisher(
+            "body_pose/raw", PoseLite, queue_size=1
+        )
+        self.pose_publisher = rospy.Publisher("body_pose", Pose, queue_size=1)
+        self.joy_subscriber = rospy.Subscriber("joy", Joy, self.joy_callback)
         self.swing_height = rospy.get_param("gait/swing_height", 0)
         self.nominal_height = rospy.get_param("gait/nominal_height", 0)
 
@@ -49,44 +54,44 @@ CTRL-C to quit
         """
 
         self.velocityBindings = {
-                'i':(1,0,0,0),
-                'o':(1,0,0,-1),
-                'j':(0,0,0,1),
-                'l':(0,0,0,-1),
-                'u':(1,0,0,1),
-                ',':(-1,0,0,0),
-                '.':(-1,0,0,1),
-                'm':(-1,0,0,-1),
-                'O':(1,-1,0,0),
-                'I':(1,0,0,0),
-                'J':(0,1,0,0),
-                'L':(0,-1,0,0),
-                'U':(1,1,0,0),
-                '<':(-1,0,0,0),
-                '>':(-1,-1,0,0),
-                'M':(-1,1,0,0),
-                'v':(0,0,1,0),
-                'n':(0,0,-1,0),
-            }
+            "i": (1, 0, 0, 0),
+            "o": (1, 0, 0, -1),
+            "j": (0, 0, 0, 1),
+            "l": (0, 0, 0, -1),
+            "u": (1, 0, 0, 1),
+            ",": (-1, 0, 0, 0),
+            ".": (-1, 0, 0, 1),
+            "m": (-1, 0, 0, -1),
+            "O": (1, -1, 0, 0),
+            "I": (1, 0, 0, 0),
+            "J": (0, 1, 0, 0),
+            "L": (0, -1, 0, 0),
+            "U": (1, 1, 0, 0),
+            "<": (-1, 0, 0, 0),
+            ">": (-1, -1, 0, 0),
+            "M": (-1, 1, 0, 0),
+            "v": (0, 0, 1, 0),
+            "n": (0, 0, -1, 0),
+        }
 
         self.poseBindings = {
-                'f':(-1,0,0,0),
-                'h':(1,0,0,0),
-                't':(0,1,0,0),
-                'b':(0,-1,0,0),
-                'r':(0,0,1,0),
-                'y':(0,0,-1,0),
-            }
+            "f": (-1, 0, 0, 0),
+            "h": (1, 0, 0, 0),
+            "t": (0, 1, 0, 0),
+            "b": (0, -1, 0, 0),
+            "r": (0, 0, 1, 0),
+            "y": (0, 0, -1, 0),
+        }
 
-        self.speedBindings={
-                'q':(1.1,1.1),
-                'z':(.9,.9),
-                'w':(1.1,1),
-                'x':(.9,1),
-                'e':(1,1.1),
-                'c':(1,.9),
-            }
-        
+        self.speedBindings = {
+            "q": (1.1, 1.1),
+            "z": (0.9, 0.9),
+            "w": (1.1, 1),
+            "x": (0.9, 1),
+            "e": (1, 1.1),
+            "c": (1, 0.9),
+        }
+
         self.poll_keys()
 
     def joy_callback(self, data):
@@ -102,7 +107,7 @@ CTRL-C to quit
         body_pose_lite = PoseLite()
         body_pose_lite.x = 0
         body_pose_lite.y = 0
-        body_pose_lite.roll = (not data.buttons[5]) *-data.axes[3] * 0.349066
+        body_pose_lite.roll = (not data.buttons[5]) * -data.axes[3] * 0.349066
         body_pose_lite.pitch = data.axes[4] * 0.174533
         body_pose_lite.yaw = data.buttons[5] * data.axes[3] * 0.436332
         if data.axes[5] < 0:
@@ -113,7 +118,9 @@ CTRL-C to quit
         body_pose = Pose()
         body_pose.position.z = body_pose_lite.z
 
-        quaternion = tf.transformations.quaternion_from_euler(body_pose_lite.roll, body_pose_lite.pitch, body_pose_lite.yaw)
+        quaternion = tf.transformations.quaternion_from_euler(
+            body_pose_lite.roll, body_pose_lite.pitch, body_pose_lite.yaw
+        )
         body_pose.orientation.x = quaternion[0]
         body_pose.orientation.y = quaternion[1]
         body_pose.orientation.z = quaternion[2]
@@ -136,8 +143,8 @@ CTRL-C to quit
 
         try:
             print(self.msg)
-            print(self.vels( self.speed, self.turn))
-            
+            print(self.vels(self.speed, self.turn))
+
             while not rospy.is_shutdown():
                 key = self.getKey()
                 if key in self.velocityBindings.keys():
@@ -145,10 +152,10 @@ CTRL-C to quit
                     y = self.velocityBindings[key][1]
                     z = self.velocityBindings[key][2]
                     th = self.velocityBindings[key][3]
-                    
+
                     if cmd_attempts > 1:
                         twist = Twist()
-                        twist.linear.x = x *self.speed
+                        twist.linear.x = x * self.speed
                         twist.linear.y = y * self.speed
                         twist.linear.z = z * self.speed
                         twist.angular.x = 0
@@ -157,19 +164,27 @@ CTRL-C to quit
                         self.velocity_publisher.publish(twist)
 
                     cmd_attempts += 1
-                    
+
                 elif key in self.speedBindings.keys():
                     self.speed = self.speed * self.speedBindings[key][0]
                     self.turn = self.turn * self.speedBindings[key][1]
-                    
+
                     print(self.vels(self.speed, self.turn))
-                    if (status == 14):
+                    if status == 14:
                         print(self.msg)
                     status = (status + 1) % 15
 
                 else:
+                    twist = Twist()
+                    twist.linear.x = 0
+                    twist.linear.y = 0
+                    twist.linear.z = 0
+                    twist.angular.x = 0
+                    twist.angular.y = 0
+                    twist.angular.z = 0
+                    self.velocity_publisher.publish(twist)
                     cmd_attempts = 0
-                    if (key == '\x03'):
+                    if key == "\x03":
                         break
 
         except Exception as e:
@@ -186,23 +201,24 @@ CTRL-C to quit
             self.velocity_publisher.publish(twist)
 
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-        
+
     def getKey(self):
         tty.setraw(sys.stdin.fileno())
         rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
         if rlist:
             key = sys.stdin.read(1)
         else:
-            key = ''
+            key = ""
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
         return key
 
     def vels(self, speed, turn):
-        return "currently:\tspeed %s\tturn %s " % (speed,turn)
+        return "currently:\tspeed %s\tturn %s " % (speed, turn)
 
     def map(self, x, in_min, in_max, out_min, out_max):
-        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
 
 if __name__ == "__main__":
-    rospy.init_node('champ_teleop')
+    rospy.init_node("champ_teleop")
     teleop = Teleop()
